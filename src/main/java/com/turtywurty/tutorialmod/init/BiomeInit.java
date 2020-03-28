4f3ca2e84181e7746b6df76eb482795429f4480e
TurtysProductions <43997792+TurtysProductions@users.noreply.github.com>
2020-03-28 14:31:06 +0000

    Added Surface Builder

--- src/main/java/com/turtywurty/tutorialmod/init/BiomeInit.java
+++ src/main/java/com/turtywurty/tutorialmod/init/BiomeInit.java
@@ -2,11 +2,15 @@ package com.turtywurty.tutorialmod.init;
 
 import com.turtywurty.tutorialmod.TutorialMod;
 import com.turtywurty.tutorialmod.world.biomes.ExampleBiome;
+import com.turtywurty.tutorialmod.world.biomes.ExampleBiomeSurfaceBuilder;
 
 import net.minecraft.block.Blocks;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.Category;
 import net.minecraft.world.biome.Biome.RainType;
+import net.minecraft.world.gen.surfacebuilders.ConfiguredSurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilders.ISurfaceBuilderConfig;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
 import net.minecraftforge.common.BiomeDictionary;
@@ -20,23 +24,34 @@ public class BiomeInit {
 	public static final DeferredRegister<Biome> BIOMES = new DeferredRegister<>(ForgeRegistries.BIOMES,
 			TutorialMod.MOD_ID);
 
-	public static final RegistryObject<Biome> EXAMPLE_BIOME = BIOMES.register("example_biome",
-			() -> new ExampleBiome(new Biome.Builder().precipitation(RainType.SNOW).scale(1.2f).temperature(0.5f)
-					.waterColor(16724639).waterFogColor(16762304)
-					.surfaceBuilder(SurfaceBuilder.DEFAULT,
-							new SurfaceBuilderConfig(BlockInit.example_block.getDefaultState(),
-									BlockInitNew.DEF_BLOCK.get().getDefaultState(),
-									Blocks.ACACIA_PLANKS.getDefaultState()))
-					.category(Category.PLAINS).downfall(0.5f).depth(0.12f).parent(null)));
+	public static final RegistryObject<Biome> EXAMPLE_BIOME = BIOMES
+			.register("example_biome",
+					() -> new ExampleBiome(
+							new Biome.Builder().precipitation(RainType.SNOW).scale(1.2f).temperature(0.5f)
+									.waterColor(16724639).waterFogColor(16762304)
+									.surfaceBuilder(
+											new ConfiguredSurfaceBuilder<SurfaceBuilderConfig>(
+													register("example_surface",
+															new ExampleBiomeSurfaceBuilder(
+																	SurfaceBuilderConfig::deserialize)),
+													new SurfaceBuilderConfig(Blocks.COARSE_DIRT.getDefaultState(),
+															Blocks.DIRT.getDefaultState(),
+															Blocks.DIRT.getDefaultState())))
+									.category(Category.PLAINS).downfall(0.5f).depth(0.12f).parent(null)));
 
 	public static void registerBiomes() {
 		registerBiome(EXAMPLE_BIOME.get(), Type.PLAINS, Type.OVERWORLD);
 	}
 
 	private static void registerBiome(Biome biome, Type... types) {
-		//the line below will make it spawn in the overworld
+		// the line below will make it spawn in the overworld
 		BiomeManager.addBiome(BiomeManager.BiomeType.COOL, new BiomeManager.BiomeEntry(biome, 100));
 		BiomeDictionary.addTypes(biome, types);
 		BiomeManager.addSpawnBiome(biome);
+	}
+
+	@SuppressWarnings("deprecation")
+	private static <C extends ISurfaceBuilderConfig, F extends SurfaceBuilder<C>> F register(String key, F builderIn) {
+		return (F) (Registry.<SurfaceBuilder<?>>register(Registry.SURFACE_BUILDER, key, builderIn));
 	}
 }
---
+++ src/main/java/com/turtywurty/tutorialmod/world/biomes/ExampleBiomeSurfaceBuilder.java
@@ -0,0 +1,39 @@
+package com.turtywurty.tutorialmod.world.biomes;
+
+import java.util.Random;
+import java.util.function.Function;
+
+import com.mojang.datafixers.Dynamic;
+import com.turtywurty.tutorialmod.init.BlockInit;
+import com.turtywurty.tutorialmod.init.BlockInitNew;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+
+public class ExampleBiomeSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderConfig> {
+
+	public ExampleBiomeSurfaceBuilder(Function<Dynamic<?>, ? extends SurfaceBuilderConfig> function) {
+		super(function);
+	}
+
+	@Override
+	public void buildSurface(Random random, IChunk chunkIn, Biome biomeIn, int x, int z, int startHeight, double noise,
+			BlockState defaultBlock, BlockState defaultFluid, int seaLevel, long seed, SurfaceBuilderConfig config) {
+		Random rd = new Random();
+		int i = rd.nextInt(3);
+		if (i == 0)
+			SurfaceBuilder.DEFAULT.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock,
+					defaultFluid, seaLevel, seed, new SurfaceBuilderConfig(BlockInit.example_block.getDefaultState(),
+							BlockInitNew.DEF_BLOCK.get().getDefaultState(), Blocks.ACACIA_PLANKS.getDefaultState()));
+		else
+			SurfaceBuilder.DEFAULT.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock,
+					defaultFluid, seaLevel, seed,
+					new SurfaceBuilderConfig(
+							i == 1 ? Blocks.GRASS_BLOCK.getDefaultState() : BlockInit.example_block.getDefaultState(),
+							BlockInitNew.DEF_BLOCK.get().getDefaultState(), Blocks.ACACIA_PLANKS.getDefaultState()));
+	}
+}
