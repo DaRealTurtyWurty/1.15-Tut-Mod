3eb92c26f10a8d8bd0113006356ce95944c919c1
TurtysProductions <43997792+TurtysProductions@users.noreply.github.com>
2020-03-28 14:33:37 +0000

    DimensionInit

---
+++ src/main/java/com/turtywurty/tutorialmod/init/DimensionInit.java
@@ -0,0 +1,16 @@
+package com.turtywurty.tutorialmod.init;
+
+import com.turtywurty.tutorialmod.TutorialMod;
+import com.turtywurty.tutorialmod.world.dimension.ExampleModDimension;
+
+import net.minecraftforge.common.ModDimension;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class DimensionInit {
+	
+	public static final DeferredRegister<ModDimension> MOD_DIMENSIONS = new DeferredRegister<>(ForgeRegistries.MOD_DIMENSIONS, TutorialMod.MOD_ID);
+	
+	public static final RegistryObject<ModDimension> EXAMPLE_DIM = MOD_DIMENSIONS.register("example_dim", () -> new ExampleModDimension());
+}
