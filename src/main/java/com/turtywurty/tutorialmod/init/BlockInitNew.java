143a1b90c75a4d31aea49be2e5187d93556eee2b
TurtysProductions <43997792+TurtysProductions@users.noreply.github.com>
2020-03-28 14:32:03 +0000

    Planks, Log, Leaves, Sapling

--- src/main/java/com/turtywurty/tutorialmod/init/BlockInitNew.java
+++ src/main/java/com/turtywurty/tutorialmod/init/BlockInitNew.java
@@ -1,11 +1,17 @@
 package com.turtywurty.tutorialmod.init;
 
 import com.turtywurty.tutorialmod.TutorialMod;
+import com.turtywurty.tutorialmod.objects.blocks.ExampleChestBlock;
 import com.turtywurty.tutorialmod.objects.blocks.ModPressurePlateBlock;
+import com.turtywurty.tutorialmod.objects.blocks.ModSaplingBlock;
 import com.turtywurty.tutorialmod.objects.blocks.ModWoodButtonBlock;
+import com.turtywurty.tutorialmod.world.feature.JazzTree;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.FenceBlock;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.block.LogBlock;
 import net.minecraft.block.PressurePlateBlock.Sensitivity;
 import net.minecraft.block.StairsBlock;
 import net.minecraft.block.material.Material;
@@ -16,12 +22,35 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class BlockInitNew {
 
-	public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
+	public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS,
+			TutorialMod.MOD_ID);
 
-	public static final RegistryObject<Block> DEF_BLOCK = BLOCKS.register("def_block", () -> new Block(Block.Properties.create(Material.IRON)));
-	
-	public static final RegistryObject<Block> EXAMPLE_STAIRS = BLOCKS.register("example_stairs", () -> new StairsBlock(() -> BlockInit.example_block.getDefaultState(), Block.Properties.create(Material.SAND, MaterialColor.GOLD)));
-	public static final RegistryObject<Block> EXAMPLE_FENCE = BLOCKS.register("example_fence", () -> new FenceBlock(Block.Properties.create(Material.SAND, MaterialColor.GOLD)));
-	public static final RegistryObject<Block> EXAMPLE_BUTTON = BLOCKS.register("example_button", () -> new ModWoodButtonBlock(Block.Properties.create(Material.SAND, MaterialColor.GOLD)));
-	public static final RegistryObject<Block> EXAMPLE_PRESSURE_PLATE = BLOCKS.register("example_pressure_plate", () -> new ModPressurePlateBlock(Sensitivity.EVERYTHING, Block.Properties.create(Material.SAND, MaterialColor.GOLD)));
+	public static final RegistryObject<Block> DEF_BLOCK = BLOCKS.register("def_block",
+			() -> new Block(Block.Properties.create(Material.IRON)));
+
+	public static final RegistryObject<Block> EXAMPLE_STAIRS = BLOCKS.register("example_stairs",
+			() -> new StairsBlock(() -> BlockInit.example_block.getDefaultState(),
+					Block.Properties.create(Material.SAND, MaterialColor.GOLD)));
+	public static final RegistryObject<Block> EXAMPLE_FENCE = BLOCKS.register("example_fence",
+			() -> new FenceBlock(Block.Properties.create(Material.SAND, MaterialColor.GOLD)));
+	public static final RegistryObject<Block> EXAMPLE_BUTTON = BLOCKS.register("example_button",
+			() -> new ModWoodButtonBlock(Block.Properties.create(Material.SAND, MaterialColor.GOLD)));
+	public static final RegistryObject<Block> EXAMPLE_PRESSURE_PLATE = BLOCKS.register("example_pressure_plate",
+			() -> new ModPressurePlateBlock(Sensitivity.EVERYTHING,
+					Block.Properties.create(Material.SAND, MaterialColor.GOLD)));
+
+	public static final RegistryObject<Block> EXAMPLE_CHEST = BLOCKS.register("example_chest",
+			() -> new ExampleChestBlock(Block.Properties.from(BlockInitNew.DEF_BLOCK.get())));
+
+	public static final RegistryObject<Block> JAZZ_PLANKS = BLOCKS.register("jazz_planks",
+			() -> new Block(Block.Properties.from(Blocks.OAK_PLANKS)));
+
+	public static final RegistryObject<Block> JAZZ_LOG = BLOCKS.register("jazz_log",
+			() -> new LogBlock(MaterialColor.WOOD, Block.Properties.from(Blocks.OAK_LOG)));
+
+	public static final RegistryObject<Block> JAZZ_LEAVES = BLOCKS.register("jazz_leaves",
+			() -> new LeavesBlock(Block.Properties.from(Blocks.OAK_LEAVES)));
+
+	public static final RegistryObject<Block> JAZZ_SAPLING = BLOCKS.register("jazz_sapling",
+			() -> new ModSaplingBlock(() -> new JazzTree(), Block.Properties.from(Blocks.OAK_SAPLING)));
 }
---
+++ src/main/java/com/turtywurty/tutorialmod/objects/blocks/ModSaplingBlock.java
@@ -0,0 +1,80 @@
+package com.turtywurty.tutorialmod.objects.blocks;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BushBlock;
+import net.minecraft.block.IGrowable;
+import net.minecraft.block.trees.Tree;
+import net.minecraft.state.IntegerProperty;
+import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.event.ForgeEventFactory;
+
+public class ModSaplingBlock extends BushBlock implements IGrowable {
+
+	public static final IntegerProperty STAGE = BlockStateProperties.STAGE_0_1;
+	protected static final VoxelShape SHAPE = Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 12.0D, 14.0D);
+	private final Supplier<Tree> tree;
+
+	public ModSaplingBlock(Supplier<Tree> treeIn, Properties properties) {
+		super(properties);
+		this.tree = treeIn;
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return SHAPE;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
+		super.tick(state, worldIn, pos, rand);
+		if (!worldIn.isAreaLoaded(pos, 1)) {
+			return;
+		}
+		if (worldIn.getLight(pos.up()) >= 9 && rand.nextInt(7) == 0) {
+			this.grow(worldIn, pos, state, rand);
+		}
+	}
+
+	public void grow(ServerWorld serverWorld, BlockPos pos, BlockState state, Random rand) {
+		if (state.get(STAGE) == 0) {
+			serverWorld.setBlockState(pos, state.cycle(STAGE), 4);
+		} else {
+			if (!ForgeEventFactory.saplingGrowTree(serverWorld, rand, pos))
+				return;
+			this.tree.get().func_225545_a_(serverWorld, serverWorld.getChunkProvider().getChunkGenerator(), pos, state,
+					rand);
+		}
+	}
+
+	@Override
+	public void grow(ServerWorld serverWorld, Random rand, BlockPos pos, BlockState state) {
+		this.grow(serverWorld, pos, state, rand);
+	}
+
+	@Override
+	public boolean canGrow(IBlockReader worldIn, BlockPos pos, BlockState state, boolean isClient) {
+		return true;
+	}
+
+	@Override
+	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, BlockState state) {
+		return (double) worldIn.rand.nextFloat() < 0.45D;
+	}
+
+	@Override
+	protected void fillStateContainer(Builder<Block, BlockState> builder) {
+		builder.add(STAGE);
+	}
+}
