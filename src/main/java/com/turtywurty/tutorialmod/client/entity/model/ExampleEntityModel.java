a77d320f48ba97c70199ac039d7d0cf437bcffa6
TurtysProductions <43997792+TurtysProductions@users.noreply.github.com>
2020-07-26 23:12:39 +0100

    Animation Tutorial

--- src/main/java/com/turtywurty/tutorialmod/client/entity/model/ExampleEntityModel.java
+++ src/main/java/com/turtywurty/tutorialmod/client/entity/model/ExampleEntityModel.java
@@ -1,132 +1,97 @@
+// Made with Blockbench 3.6.3
+// Exported for Minecraft version 1.12.2 or 1.15.2 (same format for both) for entity models animated with GeckoLib
+// Paste this class into your mod and follow the documentation for GeckoLib to use animations. You can find the documentation here: https://github.com/bernie-g/geckolib
+// Blockbench plugin created by Gecko
 package com.turtywurty.tutorialmod.client.entity.model;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.turtywurty.tutorialmod.entities.ExampleEntity;
 
-import net.minecraft.client.renderer.entity.model.EntityModel;
-import net.minecraft.client.renderer.model.ModelRenderer;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib.animation.model.AnimatedEntityModel;
+import software.bernie.geckolib.animation.render.AnimatedModelRenderer;
 
-public class ExampleEntityModel<T extends ExampleEntity> extends EntityModel<T> {
-	private final ModelRenderer Body;
-	private final ModelRenderer Legs;
-	private final ModelRenderer Back;
-	private final ModelRenderer RightBack;
-	private final ModelRenderer LeftBack;
-	private final ModelRenderer Front;
-	private final ModelRenderer RightFront;
-	private final ModelRenderer LeftFront;
-	private final ModelRenderer Head;
+public class ExampleEntityModel extends AnimatedEntityModel<ExampleEntity> {
+
+	private final AnimatedModelRenderer Body;
+	private final AnimatedModelRenderer Legs;
+	private final AnimatedModelRenderer Back;
+	private final AnimatedModelRenderer RightBack;
+	private final AnimatedModelRenderer LeftBack;
+	private final AnimatedModelRenderer Front;
+	private final AnimatedModelRenderer RightFront;
+	private final AnimatedModelRenderer LeftFront;
+	private final AnimatedModelRenderer Head;
 
 	public ExampleEntityModel() {
 		textureWidth = 64;
 		textureHeight = 64;
+		Body = new AnimatedModelRenderer(this);
+		Body.setRotationPoint(0.0F, 14.6667F, -2.3333F);
+		Body.setTextureOffset(0, 0).addBox(-6.0F, -4.6667F, -7.6667F, 12.0F, 8.0F, 18.0F, 0.0F, false);
+		Body.setModelRendererName("Body");
+		this.registerModelRenderer(Body);
 
-		Body = new ModelRenderer(this);
-		Body.setRotationPoint(0.0F, 16.0F, 0.0F);
-		Body.setTextureOffset(0, 0).addBox(-6.0F, -6.0F, -10.0F, 12, 8, 18, 0.0F, false);
-
-		Legs = new ModelRenderer(this);
-		Legs.setRotationPoint(0.0F, 0.0F, 0.0F);
+		Legs = new AnimatedModelRenderer(this);
+		Legs.setRotationPoint(0.0F, 1.3333F, 2.3333F);
 		Body.addChild(Legs);
 
-		Back = new ModelRenderer(this);
+		Legs.setModelRendererName("Legs");
+		this.registerModelRenderer(Legs);
+
+		Back = new AnimatedModelRenderer(this);
 		Back.setRotationPoint(0.0F, 0.0F, 0.0F);
 		Legs.addChild(Back);
 
-		RightBack = new ModelRenderer(this);
+		Back.setModelRendererName("Back");
+		this.registerModelRenderer(Back);
+
+		RightBack = new AnimatedModelRenderer(this);
 		RightBack.setRotationPoint(-5.0F, 2.0F, 7.0F);
 		Back.addChild(RightBack);
-		RightBack.setTextureOffset(0, 0).addBox(-1.0F, 0.0F, -1.0F, 2, 6, 2, 0.0F, false);
+		RightBack.setTextureOffset(0, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 6.0F, 2.0F, 0.0F, false);
+		RightBack.setModelRendererName("RightBack");
+		this.registerModelRenderer(RightBack);
 
-		LeftBack = new ModelRenderer(this);
+		LeftBack = new AnimatedModelRenderer(this);
 		LeftBack.setRotationPoint(5.0F, 2.0F, 7.0F);
 		Back.addChild(LeftBack);
-		LeftBack.setTextureOffset(6, 6).addBox(-1.0F, 0.0F, -1.0F, 2, 6, 2, 0.0F, false);
+		LeftBack.setTextureOffset(6, 6).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 6.0F, 2.0F, 0.0F, false);
+		LeftBack.setModelRendererName("LeftBack");
+		this.registerModelRenderer(LeftBack);
 
-		Front = new ModelRenderer(this);
+		Front = new AnimatedModelRenderer(this);
 		Front.setRotationPoint(0.0F, 0.0F, 0.0F);
 		Legs.addChild(Front);
 
-		RightFront = new ModelRenderer(this);
+		Front.setModelRendererName("Front");
+		this.registerModelRenderer(Front);
+
+		RightFront = new AnimatedModelRenderer(this);
 		RightFront.setRotationPoint(-5.0F, 2.0F, -9.0F);
 		Front.addChild(RightFront);
-		RightFront.setTextureOffset(24, 26).addBox(-1.0F, 0.0F, -1.0F, 2, 6, 2, 0.0F, false);
+		RightFront.setTextureOffset(24, 26).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 6.0F, 2.0F, 0.0F, false);
+		RightFront.setModelRendererName("RightFront");
+		this.registerModelRenderer(RightFront);
 
-		LeftFront = new ModelRenderer(this);
+		LeftFront = new AnimatedModelRenderer(this);
 		LeftFront.setRotationPoint(5.0F, 2.0F, -9.0F);
 		Front.addChild(LeftFront);
-		LeftFront.setTextureOffset(30, 32).addBox(-1.0F, 0.0F, -1.0F, 2, 6, 2, 0.0F, false);
+		LeftFront.setTextureOffset(30, 32).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 6.0F, 2.0F, 0.0F, false);
+		LeftFront.setModelRendererName("LeftFront");
+		this.registerModelRenderer(LeftFront);
 
-		Head = new ModelRenderer(this);
-		Head.setRotationPoint(0.0F, -5.0F, -10.0F);
+		Head = new AnimatedModelRenderer(this);
+		Head.setRotationPoint(0.0F, -3.6667F, -7.6667F);
 		Body.addChild(Head);
-		Head.setTextureOffset(0, 26).addBox(-3.0F, -4.0F, -6.0F, 6, 6, 6, 0.0F, false);
+		Head.setTextureOffset(0, 26).addBox(-3.0F, -4.0F, -6.0F, 6.0F, 6.0F, 6.0F, 0.0F, false);
+		Head.setModelRendererName("Head");
+		this.registerModelRenderer(Head);
+
+		this.rootBones.add(Body);
 	}
 
 	@Override
-	public void render(MatrixStack matrixStackIn, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn,
-			float red, float green, float blue, float alpha) {
-		Body.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
-	}
-
-	@Override
-	public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks,
-			float netHeadYaw, float headPitch) {
-		this.Head.rotateAngleX = headPitch * ((float) Math.PI / 180F);
-		this.Head.rotateAngleY = netHeadYaw * ((float) Math.PI / 180F);
-		//this.Body.rotateAngleZ = ((float)Math.PI / 2F);
-		this.RightBack.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-		this.LeftBack.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount;
-		this.RightFront.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount;
-		this.LeftFront.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-	}
-
-	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-		modelRenderer.rotateAngleX = x;
-		modelRenderer.rotateAngleY = y;
-		modelRenderer.rotateAngleZ = z;
-	}
-
-	@Override
-	public void setLivingAnimations(T entityIn, float limbSwing, float limbSwingAmount, float partialTick) {
-		super.setLivingAnimations(entityIn, limbSwing, limbSwingAmount, partialTick);
-	}
-
-	public ModelRenderer getBack() {
-		return Back;
-	}
-
-	public ModelRenderer getBody() {
-		return Body;
-	}
-
-	public ModelRenderer getFront() {
-		return Front;
-	}
-
-	public ModelRenderer getHead() {
-		return Head;
-	}
-
-	public ModelRenderer getLeftBack() {
-		return LeftBack;
-	}
-
-	public ModelRenderer getLeftFront() {
-		return LeftFront;
-	}
-
-	public ModelRenderer getLegs() {
-		return Legs;
-	}
-
-	public ModelRenderer getRightBack() {
-		return RightBack;
-	}
-
-	public ModelRenderer getRightFront() {
-		return RightFront;
+	public ResourceLocation getAnimationFileLocation() {
+		return new ResourceLocation("tutorialmod", "animations/exampleentity.json");
 	}
 }
